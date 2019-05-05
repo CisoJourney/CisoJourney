c009eb43cc014df331905aa54fdd6761e8c705db
holly <holly@localhost.localdomain>
2019-05-05 11:50:58 -0400

    Building initial theme

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
  <link rel="stylesheet" type="text/css" href="/main.css" />
  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
 </head>
 </head>
 <body>
