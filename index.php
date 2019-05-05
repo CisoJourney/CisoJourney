8c20a5176ed9f7088bd4eb29b163532df67af88b
holly <holly@localhost.localdomain>
2019-05-05 10:25:52 -0400

    Adding CSS elements to index

--- index.php
+++ index.php
@@ -12,19 +12,17 @@
 <div class="nav-bar">
 </div>
 <div class="content-wrapper">
-  <div class="content-center">
-    <div class="content-full center-text">
-      <h1>Cyber Security Strategy</h1>
-      <p>A platform to learn, discuss, and build your organisation's cyber security strategy.</p>
-    </div>
-    <div class="content-trio">
-      Content left
-    </div>
-    <div class="content-trio">
-      Content middle
-    </div>
-    <div class="content-trio">
-      content right
+  <div class="content">
+    <div class="content-wrapper">
+      <div class="content-trio">
+        Content left
+      </div>
+      <div class="content-trio">
+        Content middle
+      </div>
+      <div class="content-trio">
+        content right
+      </div>
     </div>
   </div>
 </div>
