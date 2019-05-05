96e176e0377dacbcef83a3fdaee0c383b5780f11
holly <holly@localhost.localdomain>
2019-05-05 16:00:20 -0400

    Building initial theme

---
+++ login.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+ <title>CISO Journey</title>
+ <link rel="stylesheet" type="text/css" href="/main.css" />
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+ <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+</head>
+</head>
+<body>
+<div class="top-bar">
+<a href="/login.php">Sign in</a> | <a href="/register.php">Register</a>
+</div>
+<!--<div class="title-bar">
+<div class="title"><span class="on-brand">CISO</span>: Journey</div>
+</div>-->
+<div class="nav-bar">
+<ul>
+<li><a href="/content.php">Risk</a></li>
+<li><a href="/content.php">Protection</a></li>
+<li><a href="/content.php">Detection</a></li>
+<li><a href="/contnt.php">Recovery</a></li>
+</ul>
+</div>
+<div class="page-wrapper">
+  <div class="content">
+    <div class="content-full center-text">
+      <h3 class="uppercase-text black-text">Cybersecurity Strategy</h3>
+      <p>A chat full of security strategy hints, tips, and discussion.</p>
+    </div>
+    <div class="content-wrapper">
+      <div class="content-full"><a href="/content.php">
+        <div class="content-block center-text">
+          <h5 class="uppercase-text spacing-text nopad-text">Strategy</h5>
+          <p class="nopad-text">Articles covering the critical foundation topics of cybersecurity; such as how to plan, manage, and implement your security plans.</p>
+        </div></a>
+      </div>
+    </div>
+    <hr>
+    <div class="content-wrapper">
+      <div class="content-full"><a href="/content.php">
+        <div class="content-block center-text">
+          <h5 class="uppercase-text spacing-text nopad-text">Risk <i class="fas fa-crown dim-text"></i></h5>
+          <p class="nopad-text">Foobar goes here to describe what the article is about, this is just a short line on what to expect from the text.</p>
+        </div></a>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
