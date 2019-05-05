99d0443cf0fb1da99dc7a34e65364e8f3e87dba4
holly <holly@localhost.localdomain>
2019-05-05 16:38:34 -0400

    Building initial theme

--- register.php
+++ register.php
@@ -25,14 +25,15 @@
 <div class="page-wrapper">
   <div class="content">
     <div class="content-full center-text">
-      <h3 class="uppercase-text black-text">Sign-in</h3>
+      <h3 class="uppercase-text black-text">Register</h3>
       <p>Sign-in or register to access member only functions!</p>
     </div>
     <div class="content-wrapper">
         <div class="login-block center-text">
-          <h5 class="uppercase-text spacing-text">Sign-in</h5>
-          <input class="login-input" type="text">
-          <input class="login-input" type="password">
+          <h5 class="uppercase-text spacing-text">Register</h5>
+          <input class="login-input" type="text" placeholder="e-mail">
+          <input class="login-input" type="password" placeholder="password">
+          <input class="login-input" type="password" placeholdre="confirm password">
           <input class="login-button" type="submit">
         </div>
       </div>
