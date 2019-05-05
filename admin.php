0d1ab1fbfaa83564bc59f5bd98900cef19b904b8
holly <holly@localhost.localdomain>
2019-05-05 16:42:16 -0400

    Building initial theme

---
+++ admin.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+ <title>CISO Journey</title>
+ <link rel="stylesheet" type="text/css" href="/main.css" />
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+ <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+</head>
+</head>
+<body>
+<div class="top-bar">
+<a href="/login.php">Sign in</a> | <a href="/register.php">Register</a>
+</div>
+<!--<div class="title-bar">
+<div class="title"><span class="on-brand">CISO</span>: Journey</div>
+</div>-->
+<div class="nav-bar">
+<ul>
+<li><a href="/content.php">Risk</a></li>
+<li><a href="/content.php">Protection</a></li>
+<li><a href="/content.php">Detection</a></li>
+<li><a href="/contnt.php">Recovery</a></li>
+</ul>
+</div>
+<div class="page-wrapper">
+  <div class="content">
+    <div class="content-full center-text">
+      <h3 class="uppercase-text black-text">Sign-in</h3>
+      <p>Sign-in or register to access member only functions!</p>
+    </div>
+    <div class="content-wrapper">
+        <div class="login-block center-text">
+          <h5 class="uppercase-text spacing-text">Sign-in</h5>
+          <input class="login-input" type="text">
+          <input class="login-input" type="password">
+          <input class="login-button" type="submit">
+        </div>
+      </div>
+  </div>
+</div>
+</body>
+</html>
---
+++ error/404.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+ <title>CISO Journey</title>
+ <link rel="stylesheet" type="text/css" href="/main.css" />
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+ <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+</head>
+</head>
+<body>
+<div class="top-bar">
+<div class="logo">Ciso: Journey</div>
+<div class="profile-buttons"><a href="/login.php">Sign in</a> | <a href="/register.php">Register</a></div>
+</div>
+<!--<div class="title-bar">
+<div class="title"><span class="on-brand">CISO</span>: Journey</div>
+</div>-->
+<div class="nav-bar">
+<ul>
+<li><a href="/content.php">Prepare</a></li>
+<li><a href="/content.php">Protect</a></li>
+<li><a href="/content.php">Detect</a></li>
+<li><a href="/contnt.php">Recover</a></li>
+</ul>
+</div>
+<div class="page-wrapper">
+  <div class="content">
+    <div class="content-full center-text">
+      <h3 class="uppercase-text black-text">Cybersecurity Strategy</h3>
+      <p>A chat full of security strategy hints, tips, and discussion.</p>
+    </div>
+    <div class="content-wrapper">
+      <div class="content-trio"><a href="/content.php">
+        <div class="content-block center-text">
+          <div class="block-icon ciso-color"><i class="fas fa-flag"></i></div>
+          <h5 class="uppercase-text spacing-text">Error 404</h5>
+          <p>Sorry - the page you requeted does not exist :(</p>
+        </div></a>
+      </div>
+      <div class="content-trio"><a href="/content.php">
+        <div class="content-block center-text">
+          <div class="block-icon training-color"><i class="fas fa-dumbbell"></i></div>
+          <h5 class="uppercase-text spacing-text">Testing</h5>
+          <p>Content geared towards penetration testing and vulnerability analysis. Had a vulnerability raised on a security test recently and want to know more? Check here.</p>
+        </div></a>
+      </div>
+      </a>
+      <div class="content-trio center-text"><a href="/content.php">
+        <div class="content-block">
+          <div class="block-icon labs-color"><i class="fas fa-flask"></i></div>
+          <h5 class="uppercase-text spacing-text">Labs</h5>
+          <p>Taking the training articles to the next level with hands-on labs that let you practise the techniques that you've learned. Want to see how an issue really works? You'll find that content here.</p>
+        </div></a>
+      </div>
+      </a>
+    </div>
+  </div>
+</div>
+</body>
+</html>
