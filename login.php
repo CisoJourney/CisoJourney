0c0bb670f661431f87cebec302c539cedf3740f4
holly <holly@localhost.localdomain>
2019-05-05 16:01:37 -0400

    Building initial theme

--- login.php
+++ login.php
@@ -25,24 +25,14 @@
 <div class="page-wrapper">
   <div class="content">
     <div class="content-full center-text">
-      <h3 class="uppercase-text black-text">Cybersecurity Strategy</h3>
-      <p>A chat full of security strategy hints, tips, and discussion.</p>
+      <h3 class="uppercase-text black-text">Sign-in</h3>
+      <p>Sign-in or register to access member only functions!</p>
     </div>
     <div class="content-wrapper">
-      <div class="content-full"><a href="/content.php">
+      <div class="content-full">
         <div class="content-block center-text">
           <h5 class="uppercase-text spacing-text nopad-text">Strategy</h5>
-          <p class="nopad-text">Articles covering the critical foundation topics of cybersecurity; such as how to plan, manage, and implement your security plans.</p>
-        </div></a>
-      </div>
-    </div>
-    <hr>
-    <div class="content-wrapper">
-      <div class="content-full"><a href="/content.php">
-        <div class="content-block center-text">
-          <h5 class="uppercase-text spacing-text nopad-text">Risk <i class="fas fa-crown dim-text"></i></h5>
-          <p class="nopad-text">Foobar goes here to describe what the article is about, this is just a short line on what to expect from the text.</p>
-        </div></a>
+        </div>
       </div>
     </div>
   </div>
