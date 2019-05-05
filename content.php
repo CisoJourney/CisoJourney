8759a050ad2cf3e490730a4b3811f762e2ffa4fd
holly <holly@localhost.localdomain>
2019-05-05 12:27:40 -0400

    Building initial theme

--- content.php
+++ content.php
@@ -29,26 +29,11 @@
       <p>A chat full of security strategy hints, tips, and discussion.</p>
     </div>
     <div class="content-wrapper">
-      <div class="content-trio"><a href="/content.php">
+      <div class="content-full"><a href="/content.php">
         <div class="content-block center-text">
           <div class="block-icon ciso-color"><i class="fas fa-flag"></i></div>
           <h5 class="uppercase-text spacing-text">Strategy</h5>
           <p>Articles covering the critical foundation topics of cybersecurity; such as how to plan, manage, and implement your security plans.</p>
-        </div></a>
-      </div>
-      <div class="content-trio"><a href="/content.php">
-        <div class="content-block center-text">
-          <div class="block-icon training-color"><i class="fas fa-dumbbell"></i></div>
-          <h5 class="uppercase-text spacing-text">Training</h5>
-          <p>Content geared towards penetration testing and vulnerability analysis. Had a vulnerability raised on a security test recently and want to know more? Check here.</p>
-        </div></a>
-      </div>
-      </a>
-      <div class="content-trio center-text"><a href="/content.php">
-        <div class="content-block">
-          <div class="block-icon labs-color"><i class="fas fa-flask"></i></div>
-          <h5 class="uppercase-text spacing-text">Labs</h5>
-          <p>Taking the training articles to the next level with hands-on labs that let you practise the techniques that you've learned. Want to see how an issue really works? You'll find that content here.</p>
         </div></a>
       </div>
       </a>
