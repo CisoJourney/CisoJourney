af019f1b01f3b4e34904ff31c8c38300c65e87bb
holly <holly@localhost.localdomain>
2019-05-05 12:07:36 -0400

    Building initial theme

--- index.php
+++ index.php
@@ -31,21 +31,21 @@
     <div class="content-wrapper">
       <div class="content-trio">
         <div class="content-block center-text">
-          <div class="block-icon"><i class="fas fa-flag"></i></div>
+          <div class="block-icon ciso-colors"><i class="fas fa-flag"></i></div>
           <h5 class="uppercase-text spacing-text">Strategy</h5>
           <p>Articles covering the critical foundation topics of cybersecurity; such as how to plan, manage, and implement your security plans.</p>
         </div>
       </div>
       <div class="content-trio">
         <div class="content-block center-text">
-          <div class="block-icon"><i class="fas fa-dumbbell"></i></div>
+          <div class="block-icon training-colors"><i class="fas fa-dumbbell"></i></div>
           <h5 class="uppercase-text spacing-text">Training</h5>
           <p>Content geared towards penetration testing and vulnerability analysis. Had a vulnerability raised on a security test recently and want to know more? Check here.</p>
         </div>
       </div>
       <div class="content-trio center-text">
         <div class="content-block">
-          <div class="block-icon"><i class="fas fa-flask"></i></div>
+          <div class="block-icon labs-colors"><i class="fas fa-flask"></i></div>
           <h5 class="uppercase-text spacing-text">Labs</h5>
           <p>Taking the training articles to the next level with hands-on labs that let you practise the techniques that you've learned. Want to see how an issue really works? You'll find that content here.</p>
         </div>
