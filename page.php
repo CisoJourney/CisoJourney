53febaffe589b4dc1fb20a70d248299181a90a94
holly <holly@localhost.localdomain>
2019-05-05 12:52:02 -0400

    Building initial theme

---
+++ page.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+ <title>CISO Journey</title>
+ <link rel="stylesheet" type="text/css" href="/main.css" />
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+ <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+</head>
+</head>
+<body>
+<div class="top-bar">
+<a href="/login.php">Sign in</a> | <a href="/register.php">Register</a>
+</div>
+<!--<div class="title-bar">
+<div class="title"><span class="on-brand">CISO</span>: Journey</div>
+</div>-->
+<div class="nav-bar">
+<ul>
+<li><a href="/content.php">Risk</a></li>
+<li><a href="/content.php">Protection</a></li>
+<li><a href="/content.php">Detection</a></li>
+<li><a href="/contnt.php">Recovery</a></li>
+</ul>
+</div>
+<div class="page-wrapper">
+  <div class="content">
+    <div class="content-full center-text">
+      <h3 class="uppercase-text black-text">Article Title Here</h3>
+      <p>A line of text describing the content of the article, and what to expect</p>
+    </div>
+    <div class="content-wrapper">
+      <div class="content-full"><a href="/content.php">
+        <div class="content-block">
+          <h5 class="uppercase-text spacing-text>Introduction</h5>
+          <p>Articles covering the critical foundation topics of cybersecurity; such as how to plan, manage, and implement your security plans.</p>
+          <p>A second paragraph with more content goes in here.</p>
+        </div></a>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
