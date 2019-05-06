a93044e4682ae4428ab9e53215393c22f9e73ff4
holly <holly@localhost.localdomain>
2019-05-06 14:42:27 -0400

    Building initial theme

---
+++ category.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/headers.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/head.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/auth.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/topbar.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
+?>
+<div class="page-wrapper">
+  <div class="content">
+    <div class="content-full center-text">
+      <h3 class="uppercase-text black-text">Cybersecurity Strategy</h3>
+      <p>A site full of security strategy hints, tips, and discussion.</p>
+    </div>
+    <div class="content-wrapper">
+Coming soon! :(
+    </div>
+  </div>
+</div>
+</body>
+</html>
