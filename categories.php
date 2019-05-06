5a9017f318ace1d59b09bf918b4dcf0cf220b5f7
holly <holly@localhost.localdomain>
2019-05-06 11:37:31 -0400

    Building initial theme

--- categories.php
+++ categories.php
@@ -16,10 +16,10 @@ $result = $mysqli->query("SELECT id,title,description,icon FROM categories;");
       <p>A chat full of security strategy hints, tips, and discussion.</p>
     </div>
 
+    <div class="content-wrapper">
 <?php
 while($row = $result->fetch_assoc()) {
 ?>
-    <div class="content-wrapper">
       <div class="content-duo">
         <div class="content-block center-text"><a href="/category.php?id=<?php print($row['id']); ?>">
           <div class="block-icon ciso-color"><i class="<?php print($row['icon']); ?>"></i></div>
@@ -27,11 +27,11 @@ while($row = $result->fetch_assoc()) {
           <p>Articles covering the critical foundation topics of cybersecurity; such as how to plan, manage, and implement your security plans.</p>
         </div></a>
       </div>
-    </div>
 
 <?php
 }
 ?>
+    </div>
   </div>
 </div>
 </body>
