0f64f942bed8bb8cf2e1a0efb37c21efe1bcd876
holly <holly@localhost.localdomain>
2019-05-06 04:52:03 -0400

    Building initial theme

--- headers.php
+++ headers.php
@@ -1,3 +1,3 @@
 <?php
-header("Content-Security-Policy: style-src 'self' https://fonts.googleapis.com https://use.fontawesome.com; font-src 'self' https://fonts.gstatic.com");
+header("Content-Security-Policy: style-src 'self' https://fonts.googleapis.com https://use.fontawesome.com; font-src 'self' https://fonts.gstatic.com https://use.fontawesome.com");
 ?>
