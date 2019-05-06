c1272e7b456e1ff660a6fca2b4fcb760b69a3cdd
holly <holly@localhost.localdomain>
2019-05-06 11:23:20 -0400

    Building initial theme

---
+++ admin/edit-category.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/headers.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/head.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/topbar.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/auth.php';
+
+if (!isset($_SESSION['privs'])) {
+  header('Location: /login.php');
+  exit();
+}
+else if ($_SESSION['privs'] < 3) {
+  header('Location: /profile.php');
+  exit();
+}
+else if (!isset($_GET['id'])) {
+  header('Location: /admin/categories.php');
+  exit();
+}
+?>
+<div class="page-wrapper">
+  <div class="content">
+    <div class="content-full center-text">
+      <h3 class="uppercase-text black-text">Administer Category</h3>
+      <p>Modifying a category...</p>
+    </div>
+    <div class="content-wrapper">
+      <div class="content-full">
+        <div class="content-block">
+          <div class="block-icon"><i class="fas fa-layer-group"></i></div>
+          <h5 class="uppercase-text center-text spacing-text">Categories</h5>
+<table>
+<tr><th class="admin-table">ID</th><th class="admin-table">Title</th></tr>
+<?php
+$stmt = $mysqli->prepare("SELECT id,title,description FROM categories WHERE id = ?;");
+$stmt->bind_param("s", $_GET['id']);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+?>
+<form method="POST" action="/admin/update-category.php">
+<input class="login-input" name="id" type="hidden" value="<?php print htmlspecialchars($row['id']); ?>">
+<input class="login-input" name="title" type="text" value="<?php print htmlspecialchars($row['title']); ?>">
+<textarea name="description><?php print htmlspecialchars($row['description']); ?></textarea>
+<input class="login-button" value="Update" type="submit">
+</form>
+
+        </div>
+      </div>
+      </a>
+    </div>
+  </div>
+</div>
+</body>
+</html>
