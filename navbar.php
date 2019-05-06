9b8c114240fe4a8fa61091e862944e819331f34b
holly <holly@localhost.localdomain>
2019-05-06 11:51:04 -0400

    Building initial theme

--- navbar.php
+++ navbar.php
@@ -4,7 +4,7 @@
 $result = $mysqli->query("SELECT id,title,url FROM topnav;");
 while($row = $result->fetch_assoc()) {
   print '<li>';
-  print '<a href="https://cisojourney.com' . htmlspecialchars($row['url']) '">';
+  print '<a href="https://cisojourney.com' . htmlspecialchars($row['url']) . '">';
   print htmlspecialchars($row['title']);
   print '</a>';
   print '</li>';
