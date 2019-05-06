fdd441fee596f969dd67f8a006369a3e036d7f81
holly <holly@localhost.localdomain>
2019-05-06 09:56:58 -0400

    Building initial theme

--- profile.php
+++ profile.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
 
-if (isset($_SESSION['email'])) {
-  print "Logged in";
+if (!isset($_SESSION['email'])) {
+  header('Location: /login.php');
+  exit();
 }
 
 include_once $_SERVER['DOCUMENT_ROOT'] . '/headers.php';
