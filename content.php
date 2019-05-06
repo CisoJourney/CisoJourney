ebaa998c44f5536cfcc950016ca1de1b4923b015
holly <holly@localhost.localdomain>
2019-05-06 14:38:05 -0400

    Building initial theme

--- content.php
+++ content.php
@@ -3,7 +3,7 @@ $result = $mysqli->query("SELECT title,description,icon FROM areas;");
 while($row = $result->fetch_assoc()) {
 ?>
       <div class="content-trio">
-        <div class="content-block center-text"><a href="/categories.php?id=<?php print(htmlspecialchars($row['id']))?>">
+        <div class="content-block center-text"><a href="/categories.php?area=<?php print(htmlspecialchars($row['id']))?>">
           <div class="block-icon"><i class="<?php print(htmlspecialchars($row['icon']))?>"></i></div>
           <h5 class="uppercase-text spacing-text"><?php print(htmlspecialchars($row['title']))?></h5>
           <p><?php print(htmlspecialchars($row['description']))?></p>
