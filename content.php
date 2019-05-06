78fdf8a6c30affa0b55c3860e772aaf4d6307cf0
holly <holly@localhost.localdomain>
2019-05-06 14:33:35 -0400

    Building initial theme

--- content.php
+++ content.php
@@ -5,8 +5,8 @@ while($row = $result->fetch_assoc()) {
       <div class="content-trio">
         <div class="content-block center-text">
           <div class="block-icon"><i class="<?php print(htmlspecialchars($row['icon']))?>"></i></div>
-          <h5 class="uppercase-text spacing-text">Strategy</h5>
-          <p>Foobar</p>
+          <h5 class="uppercase-text spacing-text"><?php print(htmlspecialchars($row['title']))?></h5>
+          <p><?php print(htmlspecialchars($row['description']))?></p>
         </div>
       </div>
 <?php
