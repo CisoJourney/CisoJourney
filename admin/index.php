67ea88b88fe5ba64c21bfdc7b9332f3c02299c33
holly <holly@localhost.localdomain>
2019-05-06 11:52:30 -0400

    Building initial theme

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
 
-include_once $_SERVER['DOCUMENT_ROOT'] .  '/head.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/headers.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/head.php'
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/auth.php';;
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/topbar.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
 
