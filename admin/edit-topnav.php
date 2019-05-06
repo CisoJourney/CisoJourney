4ef006ff500f450df391aa64492cc3a3c3fd4965
holly <holly@localhost.localdomain>
2019-05-06 12:06:23 -0400

    Building initial theme

---
+++ admin/edit-topnav.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/headers.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/head.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/topbar.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/auth.php';
+
+if (!isset($_SESSION['privs'])) {
+  header('Location: /login.php');
+  exit();
+}
+else if ($_SESSION['privs'] < 3) {
+  header('Location: /profile.php');
+  exit();
+}
+else if (!isset($_GET['nav'])) {
+  header('Location: /admin/topnav.php');
+  exit();
+}
+?>
+<div class="page-wrapper">
+  <div class="content">
+    <div class="content-full center-text">
+      <h3 class="uppercase-text black-text">Administer Menu</h3>
+      <p>Modifying a menu item...</p>
+    </div>
+    <div class="content-wrapper">
+      <div class="content-full">
+        <div class="content-block">
+          <div class="block-icon"><i class="fas fa-layer-group"></i></div>
+          <h5 class="uppercase-text center-text spacing-text">Menu</h5>
+<?php
+$stmt = $mysqli->prepare("SELECT id,title,url FROM topnav WHERE id = ?;");
+$stmt->bind_param("s", $_GET['nav']);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+?>
+<form method="POST" action="/admin/update-category.php">
+<input class="login-input" name="id" type="hidden" value="<?php print htmlspecialchars($row['id']); ?>">
+<input class="login-input" name="title" type="text" value="<?php print htmlspecialchars($row['title']); ?>">
+<input class="login-input" name="title" type="text" value="<?php print htmlspecialchars($row['url']); ?>">
+<input class="login-button" value="Update" type="submit">
+</form>
+
+        </div>
+      </div>
+      </a>
+    </div>
+  </div>
+</div>
+</body>
+</html>
