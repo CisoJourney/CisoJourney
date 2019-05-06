729b25aa32a24c3938719e74dcfbe2b3cc6517bb
holly <holly@localhost.localdomain>
2019-05-06 11:46:52 -0400

    Building initial theme

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ session_start();
 
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/headers.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/head.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/auth.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/topbar.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
 ?>
--- navbar.php
+++ navbar.php
@@ -1,8 +1,14 @@
+<?php
+$result = $mysqli->query("SELECT id,title,url FROM topnav;");
+?>
 <div class="nav-bar">
 <ul>
-<li><a href="/content.php">Prepare</a></li>
-<li><a href="/content.php">Protect</a></li>
-<li><a href="/content.php">Detect</a></li>
-<li><a href="/contnt.php">Recover</a></li>
+<?php
+while($row = $result->fetch_assoc()) {
+?>
+<li><a href="https://cisojourney.com<?php print(htmlspecialchars($row['url'])); ?>"><?php print(htmlspecialchars($row['title'])); ?></a></li>
+<?php
+}
+?>
 </ul>
 </div>
