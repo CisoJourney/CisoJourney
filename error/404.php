15d520bbdedd67089ca65cd32d8e434139cf6ac2
holly <holly@localhost.localdomain>
2019-05-06 10:28:03 -0400

    Building initial theme

--- error/404.php
+++ error/404.php
@@ -1,9 +1,9 @@
 <?php
+session_start();
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/headers.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/head.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/topbar.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
-session_start();
 ?>
 <div class="page-wrapper">
   <div class="content">
