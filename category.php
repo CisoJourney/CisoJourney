a4ab3c9d000f5cc0428255ca4128888602a6f01a
holly <holly@localhost.localdomain>
2019-05-06 16:05:21 -0400

    Building initial theme

--- category.php
+++ category.php
@@ -7,7 +7,12 @@ include_once $_SERVER['DOCUMENT_ROOT'] .  '/auth.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/topbar.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
 
-$result = $mysqli->query("SELECT area,title,description FROM articles WHERE area = 1;");
+
+$stmt = $mysqli->prepare("SELECT area,title,description FROM articles WHERE area = 1 and category = ?;");
+$stmt->bind_param("s", $_GET['id']);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
 
 ?>
 <div class="page-wrapper">
@@ -19,8 +24,9 @@ $result = $mysqli->query("SELECT area,title,description FROM articles WHERE area
     <div class="content-full">
 <?php
 while($row = $result->fetch_assoc()) {
-  print '<div class="content-block">';
-  print title;
+  print '<div class="content-block nopad-text">';
+  print '<p>' . htmlspecialchars($row['title']) . '</p>';
+  print '<p>' . htmlspecialchars($row['description']) . '</p>';
   print '</div>';
 }
 ?>
