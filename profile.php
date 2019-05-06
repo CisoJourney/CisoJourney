42a0723935851073643894d890277536a366ef86
holly <holly@localhost.localdomain>
2019-05-06 09:25:27 -0400

    Building initial theme

--- profile.php
+++ profile.php
@@ -1,8 +1,36 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT'] . '/auth.php';
 session_start();
 
 if (isset($_SESSION['email'])) {
   print "Logged in";
 }
+
+include_once $_SERVER['DOCUMENT_ROOT'] . '/auth.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/headers.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/head.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/topbar.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/navbar.php'
+
+
 ?>
+
+<div class="page-wrapper">
+  <div class="content">
+    <div class="content-full center-text">
+      <h3 class="uppercase-text black-text">Cybersecurity Strategy</h3>
+      <p>A site full of security strategy hints, tips, and discussion.</p>
+    </div>
+    <div class="content-wrapper">
+      <div class="content-full"><a href="/categories.php">
+        <div class="content-block center-text">
+          <div class="block-icon ciso-color"><i class="fas fa-flag"></i></div>
+          <h5 class="uppercase-text spacing-text">Strategy</h5>
+          <p>Articles covering the critical foundation topics of cybersecurity; such as how to plan, manage, and implement your security plans.</p>
+        </div></a>
+      </div>
+      </a>
+    </div>
+  </div>
+</div>
+</body>
+</html>
