16c4e3d9d43f583c2d5fff6a953eb397fe01e778
holly <holly@localhost.localdomain>
2019-05-06 07:23:52 -0400

    Building initial theme

--- login.php
+++ login.php
@@ -13,8 +13,8 @@ include('/var/www/CISOJourney.com/navbar.php')
         <div class="login-block center-text">
           <h5 class="uppercase-text spacing-text">Sign-in</h5>
           <form method="POST" action="process-login.php">
-            <input class="login-input" type="text" placeholder="e-mail">
-            <input class="login-input" type="password" placeholder="password">
+            <input class="login-input" name="email" type="text" placeholder="e-mail">
+            <input class="login-input" name="password" type="password" placeholder="password">
             <img id="captcha" src="/securimage/securimage_show.php" alt="CAPTCHA Image" />
             <input class="login-input" type="text" name="captcha_code" size="10" maxlength="6" placeholder="captcha code (case insensitive)" />
             <input class="login-button" value="Login" type="submit">
