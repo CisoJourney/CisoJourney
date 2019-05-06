faeeb7c98e9166fb6ab0a7367e500ec3e11d509a
holly <holly@localhost.localdomain>
2019-05-06 07:22:01 -0400

    Building initial theme

--- process-login.php
+++ process-login.php
@@ -1,5 +1,5 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT'] . auth.php');
+include_once $_SERVER['DOCUMENT_ROOT'] . '/auth.php');
 session_start();
 
 include_once $_SERVER['DOCUMENT_ROOT'] . '/securimage/securimage.php';
