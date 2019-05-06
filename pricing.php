a99f734d9101253403c8e63d098c0cea5c399823
holly <holly@localhost.localdomain>
2019-05-06 03:05:01 -0400

    Building initial theme

---
+++ pricing.php
@@ -0,0 +1,43 @@
+<?php
+include('/var/www/CISOJourney.com/head.php');
+include('/var/www/CISOJourney.com/topbar.php');
+include('/var/www/CISOJourney.com/navbar.php')
+?>
+<div class="page-wrapper">
+  <div class="content">
+    <div class="content-full center-text">
+      <h3 class="uppercase-text black-text">Member Content</h3>
+      <p>Become a member and access member-only content. This includes early access to articles, access to the full range of technical training content, and access to our hands-on labs!</p>
+    </div>
+    <div class="content-wrapper">
+      <div class="content-trio"><a href="/categories.php">
+        <div class="content-block center-text">
+          <div class="block-icon"><i class="far fa-file-alt"></i></div>
+          <h5 class="uppercase-text spacing-text">Article</h5>
+          <p>Instant access to a single article. The article will be added to your account so you can read it whenever you like!</p>
+          <p>£0.50, one time payment</p>
+        </div></a>
+      </div>
+      <div class="content-trio"><a href="/content.php">
+        <div class="content-block center-text">
+          <div class="block-icon"><i class="fas fa-book"></i></i></div>
+          <h5 class="uppercase-text spacing-text">Member</h5>
+          <p>Access to all of our articles, including new articles. Plus early access to new training content.</p>
+          <p>£5.00, monthly</p>
+        </div></a>
+      </div>
+      </a>
+      <div class="content-trio center-text"><a href="/content.php">
+        <div class="content-block">
+          <div class="block-icon"><i class="fas fa-flask"></i></div>
+          <h5 class="uppercase-text spacing-text">Labs</h5>
+          <p>Access to all of our articles and hands-on labs. Plus early access to new training content.</p>
+          <p>8.50, monthly</p>
+        </div></a>
+      </div>
+      </a>
+    </div>
+  </div>
+</div>
+</body>
+</html>
