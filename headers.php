513a7af26941e6c557d00fda867c0c1367b655fb
holly <holly@localhost.localdomain>
2019-05-06 04:54:31 -0400

    Building initial theme

--- headers.php
+++ headers.php
@@ -1,3 +1,7 @@
 <?php
+header("X-Frame-Options: DENY");
+header("X-XSS-Protection: 1; mode=block");
+header("X-Content-Type-Options: nosniff");
+header("Strict-Transport-Security: max-age=15552000; includeSubDomains");
 header("Content-Security-Policy: style-src 'self' https://fonts.googleapis.com https://use.fontawesome.com; font-src 'self' https://fonts.gstatic.com https://use.fontawesome.com");
 ?>
