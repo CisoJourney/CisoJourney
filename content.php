68168112f5698347f310beb541127ffe24b70e75
holly <holly@localhost.localdomain>
2019-05-06 14:25:05 -0400

    Building initial theme

--- content.php
+++ content.php
@@ -1,15 +1,15 @@
-<div class="content-trio">
 <?php
 $result = $mysqli->query("SELECT title,description,icon FROM areas;");
 while($row = $result->fetch_assoc()) {
 ?>
+<div class="content-trio">
   <div class="content-block center-text">
     <div class="block-icon"><i class="<?php print(htmlspecialchars($row['icon']))?>"></i></div>
       <h5 class="uppercase-text spacing-text">Strategy</h5>
       <p>Articles covering the critical foundation topics of cybersecurity; such as how to plan, manage, and implement$
     </div>
   </div>
+</div>
 <?php
 }
 ?>
-</div>
