ee7d21731f3d6fba2d76e244ffeaeba56f8d7ad8
holly <holly@localhost.localdomain>
2019-05-06 12:37:01 -0400

    Building initial theme

--- admin/update-topnav.php
+++ admin/update-topnav.php
@@ -11,22 +11,22 @@ else if ($_SESSION['privs'] < 3) {
   header('Location: /profile.php');
   exit();
 }
-else if (!isset($_POST['nav'])) {
+else if (!isset($_POST['id'])) {
   header('Location: /admin/topnav.php');
   exit();
 }
 else if (!isset($_POST['title']) or !isset($_POST['url'])) {
-  header('Location: /admin/edit-topnav.php?error=missing&nav=' . htmlspecialchars($_POST['nav']));
+  header('Location: /admin/edit-topnav.php?error=missing&nav=' . htmlspecialchars($_POST['id']));
   exit();
 }
 else if ($_POST['title'] == "" or $_POST['url'] == "") {
-  header('Location: /admin/edit-topnav.php?error=blank&nav=' . htmlspecialchars($_POST['nav']));
+  header('Location: /admin/edit-topnav.php?error=blank&nav=' . htmlspecialchars($_POST['id']));
   exit();
 }
 
 $stmt = $mysqli->prepare("UPDATE topnav SET title = ?, url = ? WHERE id = ?;");
-$stmt->bind_param("sss", $_POST['title'], $_POST['url'], $_POST['nav']);
+$stmt->bind_param("sss", $_POST['title'], $_POST['url'], $_POST['id']);
 $stmt->execute();
-header('Location: /admin/edit-topnav.php?nav=' . htmlspecialchars($_POST['nav']));
+header('Location: /admin/edit-topnav.php?nav=' . htmlspecialchars($_POST['id']));
 exit();
 ?>
