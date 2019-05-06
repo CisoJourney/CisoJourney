bfebd0097841cd018df598ada53fbd1091b16014
holly <holly@localhost.localdomain>
2019-05-06 12:57:45 -0400

    Building initial theme

--- admin/categories.php
+++ admin/categories.php
@@ -28,16 +28,19 @@ else if ($_SESSION['privs'] < 3) {
           <div class="block-icon"><i class="fas fa-layer-group"></i></div>
           <h5 class="uppercase-text center-text spacing-text">Categories</h5>
 <table>
-<tr><th class="admin-table">ID</th><th class="admin-table">Title</th></tr>
+<tr><th class="admin-table">ID</th><th class="admin-table">Title</th><th></th></tr>
 <?php
 $result = $mysqli->query("SELECT * FROM categories;");
 while($row = $result->fetch_assoc()) {
   print '<tr>';
   print '<td class="admin-table">';
-  print '<a href="/admin/edit-category.php?category=' . htmlspecialchars($row['id']) . '">' . htmlspecialchars($row['id']) . '</a>';
+  print htmlspecialchars($row['id']);
   print '</td>';
   print '<td class="admin-table">';
   print htmlspecialchars($row['title']);
+  print '</td>';
+  print '<td class="admin-table">';
+  print '<a href="/admin/edit-category.php?category=' . htmlspecialchars($row['id']) . '"><input type="submit" value="edit"></a>';
   print '</td>';
   print '</tr>';
 }
