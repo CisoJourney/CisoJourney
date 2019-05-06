9eb8da9b87ba7335b936bd78888f3146c7eab331
holly <holly@localhost.localdomain>
2019-05-06 03:22:33 -0400

    Building initial theme

--- admin.php
+++ admin.php
@@ -13,35 +13,35 @@ include('/var/www/CISOJourney.com/navbar.php')
       <div class="content-quatro">
         <div class="content-block">
           <div class="block-icon"><i class="fas fa-users"></i></div>
-          <h5 class="uppercase-text spacing-text">Users</h5>
+          <h5 class="uppercase-text center-text spacing-text">Users</h5>
           <p></p>
         </div>
       </div>
       <div class="content-quatro">
         <div class="content-block">
           <div class="block-icon"><i class="fas fa-layer-group"></i></div>
-          <h5 class="uppercase-text spacing-text">Categories</h5>
+          <h5 class="uppercase-text center-text spacing-text">Categories</h5>
           <p></p>
         </div>
       </div>
       <div class="content-quatro">
         <div class="content-block">
           <div class="block-icon"><i class="fas fa-book"></i></div>
-          <h5 class="uppercase-text spacing-text">Articles</h5>
+          <h5 class="uppercase-text center-text spacing-text">Articles</h5>
           <p></p>
         </div>
       </div>
       <div class="content-quatro">
         <div class="content-block">
           <div class="block-icon"><i class="fas fa-align-justify"></i></div>
-          <h5 class="uppercase-text spacing-text">Content</h5>
+          <h5 class="uppercase-text center-text spacing-text">Content</h5>
           <p></p>
         </div>
       </div>
       <div class="content-quatro">
         <div class="content-block">
           <div class="block-icon"><i class="fas fa-flask"></i></div>
-          <h5 class="uppercase-text spacing-text">Labs</h5>
+          <h5 class="uppercase-text center-text spacing-text">Labs</h5>
           <p></p>
         </div>
       </div>
