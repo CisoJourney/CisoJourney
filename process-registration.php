5e0f588733a7509ee846d66f9b9e44e3a3e28189
holly <holly@localhost.localdomain>
2019-05-06 04:31:19 -0400

    Building initial theme

--- process-registration.php
+++ process-registration.php
@@ -1,11 +1,14 @@
 <?php
 if (!isset($_POST['email']) or !isset($_POST['password']) or !isset($_POST['confirm'])) {
-  header('https://cisojourney.com/register.php?error=missing');
+  header('Location: https://cisojourney.com/register.php?error=missing');
+  exit();
 }
 else if ($_POST['email'] == "" or $_POST['password'] == "" or $_POST['confirm'] == "") {
-  header('https://cisojourney.com/register.php?error=blank');
+  header('Location: https://cisojourney.com/register.php?error=blank');
+  exit();
 }
 else if ($_POST['password'] != $_POST['confirm']) {
-  header('https://cisojourney.com/register.php?error=match');
+  header('Location: https://cisojourney.com/register.php?error=match');
+  exit();
 }
 ?>
