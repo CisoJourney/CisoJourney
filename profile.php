8f8a64b8786de4bfbd14e2279ee2b1952c37e29a
holly <holly@localhost.localdomain>
2019-05-06 09:57:52 -0400

    Building initial theme

--- profile.php
+++ profile.php
@@ -18,13 +18,12 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/navbar.php'
 <div class="page-wrapper">
   <div class="content">
     <div class="content-full center-text">
-      <h3 class="uppercase-text black-text">Cybersecurity Strategy</h3>
-      <p>A site full of security strategy hints, tips, and discussion.</p>
+      <h3 class="uppercase-text black-text">Profile</h3>
+      <p>Update your settings or check your subscription status!</p>
     </div>
     <div class="content-wrapper">
       <div class="content-full"><a href="/categories.php">
         <div class="content-block center-text">
-          <div class="block-icon ciso-color"><i class="fas fa-flag"></i></div>
           <h5 class="uppercase-text spacing-text">Strategy</h5>
           <p>Articles covering the critical foundation topics of cybersecurity; such as how to plan, manage, and implement your security plans.</p>
         </div></a>
