b83d50edb19292b856b2439fb3e89e758b2de664
holly <holly@localhost.localdomain>
2019-05-07 16:15:21 -0400

    Building initial theme

--- admin/new-article.php
+++ admin/new-article.php
@@ -28,8 +28,8 @@ else if ($_SESSION['privs'] < 3) {
     <div class="content-wrapper">
       <div class="content-full">
         <div class="content-block">
-          <div class="block-icon"><i class="fas fa-layer-group"></i></div>
-          <h5 class="uppercase-text center-text spacing-text">Categories</h5>
+          <div class="block-icon"><i class="fas fa-book"></i></div>
+          <h5 class="uppercase-text center-text spacing-text">Articles</h5>
           <p class="red-text">
 <?php
 if (isset($_GET['error'])) {
