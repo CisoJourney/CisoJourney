80beadcaa6c52c19d7abe4537232fa1227b8870b
holly <holly@localhost.localdomain>
2019-05-07 09:39:44 -0400

    Building initial theme

--- navbar.php
+++ navbar.php
@@ -1,4 +1,12 @@
-<div class="<?php if (isset($_GET['area']) and $_GET['area'] == 1) { print 'ciso-area '; } ?>nav-bar">
+<div class="
+<?php 
+if (isset($_GET['area']) {
+  if ($_GET['area'] == 1) { print 'ciso-area '; }
+  else if ($_GET['area'] == 2) { print 'pentest-area '; }
+  else if ($_GET['area'] == 3) { print 'labs-area '; }
+}
+?>
+nav-bar">
   <ul>
 <?php
 $result = $mysqli->query("SELECT id,title,url FROM topnav;");
