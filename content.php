fa0b4c1695beeda3e5212835438772770cb97be7
holly <holly@localhost.localdomain>
2019-05-07 15:01:26 -0400

    Building initial theme

--- content.php
+++ content.php
@@ -7,6 +7,10 @@ while($row = $result->fetch_assoc()) {
           <div class="block-icon"><i class="<?php print(htmlspecialchars($row['icon']))?>"></i></div>
           <h5 class="uppercase-text spacing-text"><?php print(htmlspecialchars($row['title']))?></h5>
           <p><?php print(htmlspecialchars($row['description']))?></p>
+        <?php
+// TODO: HACK this is a hack to show that labs is 
+if ($row['title'] == 'Labs') { print 'Coming Soon!';} 
+?>
         </div></a>
       </div>
 <?php
