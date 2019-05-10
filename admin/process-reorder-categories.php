5a2829d45b3c177c73076d7b317410dee681669f
holly <holly@localhost.localdomain>
2019-05-10 11:07:40 -0400

    Building initial theme

--- admin/process-reorder-categories.php
+++ admin/process-reorder-categories.php
@@ -1,4 +1,7 @@
 <?php
+// Check each array item for duplictions, by iterating through the supplied
+// variables and counting each occurance. If total occurences is ever > 1
+// we have a duplicate so throw.
 foreach ($_POST as $post) {
   $count = 0;
   foreach ($_POST as $current) {
@@ -6,8 +9,17 @@ foreach ($_POST as $post) {
       $count++;
     }
   }
-  print $post . ' ' . $count . '<br />';
+  if ($count > 1) {
+    header('Location: /admin/reorder-categories.php?error=dupe');
+    exit();
+  }
 }
 
-
+foreach ($_POST as $catNum => $catNewPosition) {
+  $stmt = $mysqli->prepare("UPDATE categories SET colOrder = ? WHERE id = ?;");
+  $stmt->bind_param("ii", intval($_POST[$catNewPosition]), intval($catNum));
+  $stmt->execute();
+  header('Location: /admin/reorder-categories.php?reordered=true');
+  exit();
+}
 ?>
