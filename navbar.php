78708df9d6ec80724f1723743aca1bb70e27a9a9
holly <holly@localhost.localdomain>
2019-05-10 10:33:46 -0400

    Building initial theme

--- navbar.php
+++ navbar.php
@@ -11,7 +11,7 @@ if (isset($_GET['area'])) {
 ?>
   </div>
 </div>
-<div class="nav-bar">
+<div class="area-bar">
   <ul>
 <?php
 $result = $mysqli->query("SELECT id,title,description,icon FROM areas;");
