007c29fcda7ed58675f87506803c2b775bf70931
holly <holly@Unknown-f4-8c-50-85-08-90.home>
2019-05-11 06:11:28 -0400

    Refactored the index page

--- index.php
+++ index.php
@@ -13,7 +13,25 @@ include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
       <p>A site full of security strategy hints, tips, and discussion.</p>
     </div>
     <div class="content-wrapper">
-<?php include_once $_SERVER['DOCUMENT_ROOT'] .  '/content.php'; ?>
+<?php
+$result = $mysqli->query("SELECT id,title,description,icon FROM areas;");
+while($row = $result->fetch_assoc()) {
+?>
+      <div class="content-trio">
+        <div class="content-block center-text"><a href="/categories.php?area=<?php print(htmlspecialchars($row['id']))?>">
+        <?php
+// TODO: HACK this is a hack to show that labs is
+if ($row['title'] == 'Labs') { print '<div class="labs-comingsoon">Coming Soon!</div>'; }
+?>
+          <div class="block-icon"><i class="<?php print(htmlspecialchars($row['icon']))?>"></i></div>
+          <h5 class="uppercase-text spacing-text"><?php print(htmlspecialchars($row['title']))?></h5>
+          <p><?php print(htmlspecialchars($row['description']))?></p>
+        </div></a>
+      </div>
+<?php
+}
+?>
+
     </div>
   </div>
 </div>
