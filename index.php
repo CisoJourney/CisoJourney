8abcbd245a674b4a889068216fe41db2ed250082
holly <holly@localhost.localdomain>
2019-05-11 13:08:20 -0400

    adding hide-article funciton

--- index.php
+++ index.php
@@ -39,28 +39,36 @@ function drawFrontPage($mysqli) {
       <?php drawFrontPage($mysqli); ?>
     </div>
     <!-- Simulate some content to play with styles -->
-    <div class="content-duo center-text">
-      <div>
-        <h3 class="uppercase-text black-text">Most Popular Posts</h3>
-         <div class="content-block">
-           <h3 class="black-text nopad-text">Article title here</h3>
-           <p class="nopad-text">Article content goes here</p>
-         </div>
-         <div class="content-block">
-           <h3 class="black-text nopad-text">Article title here</h3>
-           <p class="nopad-text">Article content goes here</p>
-         </div>
+    <div class="content-wrapper">
+      <div class="content-duo">
+        <h3 class="uppercase-text center-text black-text">Most Popular Posts</h3>
+        <div class="content-block">
+          <h3 class="black-text nopad-text">Article title here</h3>
+          <p class="nopad-text">Article content goes here</p>
+        </div>
+        <div class="content-block">
+          <h3 class="black-text nopad-text">Article title here</h3>
+          <p class="nopad-text">Article content goes here</p>
+        </div>
+        <div class="content-block">
+          <h3 class="black-text nopad-text">Article title here</h3>
+          <p class="nopad-text">Article content goes here</p>
+        </div>
       </div>
-      <div>
+      <div class="content-duo">
         <h3 class="uppercase-text black-text">Most Popular Posts</h3>
-         <div class="content-block">
-           <h3 class="black-text nopad-text">Article title here</h3>
-           <p class="nopad-text">Article content goes here</p>
-         </div>
-         <div class="content-block">
-           <h3 class="black-text nopad-text">Article title here</h3>
-           <p class="nopad-text">Article content goes here</p>
-         </div>
+        <div class="content-block">
+          <h3 class="black-text nopad-text">Article title here</h3>
+          <p class="nopad-text">Article content goes here</p>
+        </div>
+        <div class="content-block">
+          <h3 class="black-text nopad-text">Article title here</h3>
+          <p class="nopad-text">Article content goes here</p>
+        </div>
+        <div class="content-block">
+          <h3 class="black-text nopad-text">Article title here</h3>
+          <p class="nopad-text">Article content goes here</p>
+        </div>
       </div>
     </div>
   </div>
