d5dd9bfcfdfa67275dafa1d315283fe2ec5c4e43
holly <holly@Unknown-f4-8c-50-85-08-90.home>
2019-05-11 07:14:53 -0400

    Refactored category

--- category.php
+++ category.php
@@ -7,6 +7,22 @@ include_once $_SERVER['DOCUMENT_ROOT'] .  '/auth.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/topbar.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
 
+// Area variable from navbar.php
+function drawCategoryDesc($mysqli, $area) {
+  $stmt = $mysqli->prepare("SELECT title,description FROM area WHERE area = ?;");
+  $stmt->bind_param("i", $area);
+  $stmt->execute();
+  $result = $stmt->get_result();
+
+  while($row = $result->fetch_assoc()) {
+    $title = htmlspecialchars($row['title']);
+    $desc  = htmlspecialchars($row['description']);
+
+    print '<h3 class="uppercase-text black-text">' . $title . '</h3>
+    print '<p>' . $desc . '</p>';
+  }
+}
+
 function drawCategory($mysqli) {
   $stmt = $mysqli->prepare("SELECT area,title,description FROM articles WHERE area = 1 and category = ?;");
   $stmt->bind_param("i", intval($_GET['id']));
@@ -27,8 +43,7 @@ function drawCategory($mysqli) {
 <div class="page-wrapper">
   <div class="content">
     <div class="content-full center-text">
-      <h3 class="uppercase-text black-text">Cybersecurity Strategy</h3>
-      <p>A site full of security strategy hints, tips, and discussion.</p>
+      <?php drawCategoryDesc(); ?>
     </div>
     <div class="content-full">
       <?php drawCategory($mysqli); ?>
