a69b9cf6b743f847f54fd6b1a54c5a87b9381507
holly <holly@localhost.localdomain>
2019-05-11 18:28:38 -0400

    working on firefox CSP bug, blocking fnt

--- headers.php
+++ headers.php
@@ -4,5 +4,6 @@ header("X-XSS-Protection: 1; mode=block");
 header("X-Content-Type-Options: nosniff");
 header("Strict-Transport-Security: max-age=15552000; includeSubDomains");
 header("Referrer-Policy: strict-origin-when-cross-origin");
-header("Content-Security-Policy: font-src 'self' https://fonts.gstatic.com https://use.fontawesome.com; style-src 'self' https://fonts.googleapis.com https://use.fontawesome.com;");
+// Had to add cisojourney.com as a font-src because Firefox is a moron and 'self' doesn't work
+header("Content-Security-Policy: font-src 'self' https://cisojourney.com https://fonts.gstatic.com https://use.fontawesome.com; style-src 'self' https://fonts.googleapis.com https://use.fontawesome.com;");
 ?>
