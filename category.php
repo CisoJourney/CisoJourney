2703aab797c3c99838edfd460e5c82f265b8961d
holly <holly@Unknown-f4-8c-50-85-08-90.home>
2019-05-11 06:47:05 -0400

    Refactored category

--- category.php
+++ category.php
@@ -7,12 +7,22 @@ include_once $_SERVER['DOCUMENT_ROOT'] .  '/auth.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/topbar.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
 
+function drawCategory($mysqli) {
+  $stmt = $mysqli->prepare("SELECT area,title,description FROM articles WHERE area = 1 and category = ?;");
+  $stmt->bind_param("i", intval($_GET['id']));
+  $stmt->execute();
+  $result = $stmt->get_result();
 
-$stmt = $mysqli->prepare("SELECT area,title,description FROM articles WHERE area = 1 and category = ?;");
-$stmt->bind_param("i", intval($_GET['id']));
-$stmt->execute();
-$result = $stmt->get_result();
+  while($row = $result->fetch_assoc()) {
+    $title = htmlspecialchars($row['title']);
+    $desc  = htmlspecialchars($row['description']);
 
+    print '<div class="content-block">';
+    print '<h5 class="nopad-text">' . $title . '</h5>';
+    print '<p class="nopad-text">' . $desc . '</p>';
+    print '</div>';
+  }
+}
 ?>
 <div class="page-wrapper">
   <div class="content">
@@ -21,14 +31,7 @@ $result = $stmt->get_result();
       <p>A site full of security strategy hints, tips, and discussion.</p>
     </div>
     <div class="content-full">
-<?php
-while($row = $result->fetch_assoc()) {
-  print '<div class="content-block">';
-  print '<h5 class="nopad-text">' . htmlspecialchars($row['title']) . '</h5>';
-  print '<p class="nopad-text">' . htmlspecialchars($row['description']) . '</p>';
-  print '</div>';
-}
-?>
+      <?php drawCategory($mysqli); ?>
     </div>
   </div>
 </div>
