9d9569b87fcadedc69a1acb63a150a59c5dd10f0
holly <holly@localhost.localdomain>
2019-05-12 17:04:20 -0400

    Implementing new function clean()

--- admin/edit-article.php
+++ admin/edit-article.php
@@ -1,24 +1,16 @@
 <?php
-session_start();
-
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/session.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/headers.php';
-include_once $_SERVER['DOCUMENT_ROOT'] .  '/head.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/auth.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/functions.php';
+
+if      ($_SESSION['privs'] < 3)   { softRedirect('/profile.php'); }
+else if (!isset($_GET['article'])) { softRedirect('/admin/articles.php'); }
+
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/head.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/topbar.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
 
-if (!isset($_SESSION['privs'])) {
-  header('Location: /login.php');
-  exit();
-}
-else if ($_SESSION['privs'] < 3) {
-  header('Location: /profile.php');
-  exit();
-}
-else if (!isset($_GET['article'])) {
-  header('Location: /admin/articles.php');
-  exit();
-}
 ?>
 <div class="page-wrapper">
   <div class="content">
@@ -33,54 +25,51 @@ else if (!isset($_GET['article'])) {
         <div class="content-block">
           <div class="block-icon"><i class="fas fa-book"></i></div>
           <h5 class="uppercase-text center-text spacing-text">Articles</h5>
-<p class="red-text">
 <?php
 if (isset($_GET['error'])) {
-  if ($_GET['error'] == 'missing') {
-    print('Oops, all fields are required!');
-  }
-  else if ($_GET['error'] == 'blank') {
-    print('Oops, all fields are required!');
+  if ($_GET['error'] == 'missing' or $_GET['error'] == 'blank') {
+    print('<p class="red-text">Oops, all fields are required!</p>');
   }
 }
-?>
-</p>
-<p class="black-text">
-<?php
+
 if (isset($_GET['updated'])) {
   if ($_GET['updated'] == 'true') {
-    print('Updated!');
+    print('<p class="black-text">Updated!</p>');
   }
 }
-?>
-</p>
-<?php
-$stmt = $mysqli->prepare("SELECT area,id,title,description,content,premium,category,display FROM articles WHERE id = ?;");
-$stmt->bind_param("s", $_GET['article']);
-$stmt->execute();
-$result = $stmt->get_result();
+
+$result = execPrepare($mysqli, "SELECT area,id,title,description,content,premium,category,display FROM articles WHERE id = ?;", array("i", $_GET['article']));
 $row = $result->fetch_assoc();
-?></p>
+
+$id      = clean($row['id']);
+$title   = clean($row['title']);
+$desc    = clean($row['description']);
+$content = clean($row['content']);
+$premium = clean($row['premium']);
+$display = clean($row['display']);
+$category = clean($row['category']);
+
+?>
 <form method="POST" action="/admin/update-article.php">
 <select class="login-input" name="area">
   <option <?php if($row['area'] == 1) { print "selected"; } ?> value="1">Strategy</option>
   <option <?php if($row['area'] == 2) { print "selected"; } ?> value="2">Testing</option>
   <option <?php if($row['area'] == 3) { print "selected"; } ?> value="3">Labs</option>
 </select>
-<input class="login-input" name="id" type="hidden" value="<?php print htmlspecialchars($row['id']); ?>">
-<input class="login-input" name="title" type="text" value="<?php print htmlspecialchars($row['title']); ?>">
-<textarea class="login-input" name="description"><?php print htmlspecialchars($row['description']); ?></textarea>
-<textarea class="login-input" name="content"><?php print htmlspecialchars($row['content']); ?></textarea>
+<input class="login-input" name="id" type="hidden" value="<?php print $id; ?>">
+<input class="login-input" name="title" type="text" value="<?php print $title; ?>">
+<textarea class="login-input" name="description"><?php print $description; ?></textarea>
+<textarea class="login-input" name="content"><?php print $content; ?></textarea>
 <select class="login-input" name="premium">
-  <option <?php if($row['premium'] == 0) { print "selected"; } ?> value="0">Not Premium</option>
-  <option <?php if($row['premium'] == 1) { print "selected"; } ?> value="1">Member</option>
-  <option <?php if($row['premium'] == 2) { print "selected"; } ?> value="2">Labs</option>
+  <option <?php if($premium == 0) { print "selected"; } ?> value="0">Not Premium</option>
+  <option <?php if($premium == 1) { print "selected"; } ?> value="1">Member</option>
+  <option <?php if($premium == 2) { print "selected"; } ?> value="2">Labs</option>
 </select>
 <select class="login-input" name="display">
-  <option <?php if($row['display'] == 0) { print "selected"; } ?> value="0">Hidden</option>
-  <option <?php if($row['display'] == 1) { print "selected"; } ?> value="1">Published</option>
+  <option <?php if($display == 0) { print "selected"; } ?> value="0">Hidden</option>
+  <option <?php if($display == 1) { print "selected"; } ?> value="1">Published</option>
 </select>
-<input class="login-input" name="category" type="text" value="<?php print htmlspecialchars($row['category']); ?>">
+<input class="login-input" name="category" type="text" value="<?php print $category; ?>">
 <input class="login-button" value="Update" type="submit">
 </form>
 
