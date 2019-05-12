908bd128f50930ad9f70c74543e4810f080a8cfe
holly <holly@localhost.localdomain>
2019-05-12 16:14:09 -0400

    Implementing new function clean()

---
+++ admin/confirmdelete-topnav.php
@@ -0,0 +1,18 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/session.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/auth.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/functions.php';
+
+if ($_SESSION['privs'] < 3) {
+  softRedirect('/profile.php');
+}
+else if (!isset($_POST['nav'])) {
+  softRedirect('/admin/users.php');
+}
+else if ($_POST['nav'] == "") {
+  softRedirect('/admin/delete-topnav.php?error=blank');
+}
+
+$result = execPrepare($mysqli, "DELETE FROM topnav WHERE id = ?;", array("i", $_GET['nav']));
+softRedirect('/admin/topnav.php?deleted=true&nav=' . $_POST['nav']);
+?>
