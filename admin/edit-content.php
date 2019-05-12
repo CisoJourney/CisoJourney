e9d8494c10693a133129b9f66bca93e38f129ef1
holly <holly@localhost.localdomain>
2019-05-12 07:44:13 -0400

    Fixing content page, with new function clean()

--- admin/edit-content.php
+++ admin/edit-content.php
@@ -1,24 +1,16 @@
 <?php
-session_start();
-
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/session.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/headers.php';
-include_once $_SERVER['DOCUMENT_ROOT'] .  '/head.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/auth.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/functions.php';
+
+if ($_SESSION['privs'] < 3)        { softRedirect('/profile.php'); }
+else if (!isset($_GET['content'])) { softRedirect('/admin/contents.php'); }
+
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/head.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/topbar.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
 
-if (!isset($_SESSION['privs'])) {
-  header('Location: /login.php');
-  exit();
-}
-else if ($_SESSION['privs'] < 3) {
-  header('Location: /profile.php');
-  exit();
-}
-else if (!isset($_GET['content'])) {
-  header('Location: /admin/contents.php');
-  exit();
-}
 ?>
 <div class="page-wrapper">
   <div class="content">
@@ -33,39 +25,31 @@ else if (!isset($_GET['content'])) {
         <div class="content-block">
           <div class="block-icon"><i class="fas fa-align-justify"></i></div>
           <h5 class="uppercase-text center-text spacing-text">Content</h5>
-<p class="red-text">
 <?php
 if (isset($_GET['error'])) {
-  if ($_GET['error'] == 'missing') {
-    print('Oops, all fields are required!');
-  }
-  else if ($_GET['error'] == 'blank') {
-    print('Oops, all fields are required!');
+  if ($_GET['error'] == 'missing' or $_GET['error'] == 'blank') {
+    print('<p class="red-text">Oops, all fields are required!</p>');
   }
 }
-?>
-</p>
-<p class="black-text">
-<?php
 if (isset($_GET['updated'])) {
   if ($_GET['updated'] == 'true') {
-    print('Updated!');
+    print('<p class="black-text">Updated!</p>');
   }
 }
-?>
-</p>
-<?php
-$stmt = $mysqli->prepare("SELECT id,title,description,icon FROM areas WHERE id = ?;");
-$stmt->bind_param("s", $_GET['content']);
-$stmt->execute();
-$result = $stmt->get_result();
+
+$result = execPrepare($mysqli, "SELECT id,title,description,icon FROM areas WHERE id = ?;", array("s", $_GET['content']));
 $row = $result->fetch_assoc();
+
+$id    = clean($row['id']);
+$title = clean($row['title']);
+$desc  = clean($row['description']);
+$icon  = clean($row['icon']);
 ?>
 <form method="POST" action="/admin/update-content.php">
-<input class="login-input dim-input" name="id" value="<?php print htmlspecialchars($row['id']); ?>" readonly>
-<input class="login-input" name="title" value="<?php print htmlspecialchars($row['title']); ?>">
-<input class="login-input" name="description" value="<?php print htmlspecialchars($row['description']); ?>">
-<input class="login-input" name="icon" value="<?php print htmlspecialchars($row['icon']); ?>">
+<input class="login-input dim-input" name="id" value="<?php print $id; ?>" readonly>
+<input class="login-input" name="title" value="<?php print $title; ?>">
+<input class="login-input" name="description" value="<?php print $desc; ?>">
+<input class="login-input" name="icon" value="<?php print $icon; ?>">
 <input class="login-button" value="Update" type="submit">
 </form>
 
