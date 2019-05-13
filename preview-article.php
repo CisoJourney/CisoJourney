d20c3e59e6c2ae9443630dd0cfc740e6c9eb6750
holly <holly@localhost.localdomain>
2019-05-13 16:56:27 -0400

    improving article display support

---
+++ preview-article.php
@@ -0,0 +1,49 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/session.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/headers.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/auth.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/functions.php';
+
+if	($_SESSION['privs'] < 3)   { softRedirect('/profile.php'); }
+else if (!isset($_GET['article'])) { softRedirect('/admin/articles.php'); }
+
+if (isset($_GET['slug'])) {
+  $result = execPrepare($mysqli, "SELECT area FROM articles WHERE slug = ?;", array("s", $_GET['slug']));
+  $row = $result->fetch_assoc();
+  $area = $row['id'];
+}
+else {
+  softRedirect('/error/404/');
+}
+
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/head.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/topbar.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
+
+function drawArticle($mysqli, $area) {
+  // TODO: ID is caps here but not in other tables
+  $result = execPrepare($mysqli, "SELECT title,description,content FROM articles WHERE slug = ?;", array("s", $_GET['slug']));
+  while($row = $result->fetch_assoc()) {
+    $title   = clean($row['title']);
+    $desc    = clean($row['description']);
+    $content = display($row['content']);
+
+    print '<div class="content-full center-text">';
+    print '<h1 class="black-text nopad-text">' . $title . '</h1>';
+    print '<p class="nopad-text">' . $desc . '</p>';
+    print '</div>';
+    print '<div class="content-full">';
+    print '<div class="content-block">';
+    print '<p class="nopad-text">' . $content . '</p>';
+    print '</div>';
+    print '</div>';
+  }
+}
+?>
+<div class="page-wrapper">
+  <div class="content">
+    <?php drawArticle($mysqli, $area); ?>
+  </div>
+</div>
+</body>
+</html>
