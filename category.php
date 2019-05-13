f3a1181a3c60e345ad3db3b226b171e77aff76ca
holly <holly@localhost.localdomain>
2019-05-13 09:27:06 -0400

    correcting a bug in category slugs

--- category.php
+++ category.php
@@ -8,7 +8,12 @@ include_once $_SERVER['DOCUMENT_ROOT'] .  '/topbar.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
 
 function drawCategoryDesc($mysqli) {
-  $result = execPrepare($mysqli, "SELECT title,description FROM categories WHERE id = ? AND hidden = 0;", array("i", $_GET['id']));
+  if (isset($_GET['slug'])) {
+    $result = execPrepare($mysqli, "SELECT id,area,title,description FROM articles WHERE category = (SELECT id FROM categories WHERE slug = ?) AND hidden = 0;", array("s", $_GET['slug']));
+  }
+  else {
+    $result = execPrepare($mysqli, "SELECT title,description FROM categories WHERE id = ? AND hidden = 0;", array("i", $_GET['id']));
+  }
   while($row = $result->fetch_assoc()) {
     $title = htmlspecialchars($row['title']);
     $desc  = htmlspecialchars($row['description']);
@@ -19,7 +24,12 @@ function drawCategoryDesc($mysqli) {
 }
 
 function drawCategory($mysqli, $area) {
-  $result = execPrepare($mysqli, "SELECT id,area,title,description FROM articles WHERE area = ? AND category = ? AND hidden = 0;", array("ii", $area, $_GET['id']));
+  if (isset($_GET['slug'])) {
+    $result = execPrepare($mysqli, "SELECT id,area,title,description FROM articles WHERE category = (SELECT id FROM categories WHERE slug = ?) AND hidden = 0;", array("s", $_GET['slug']));
+  }
+  else {
+    $result = execPrepare($mysqli, "SELECT id,area,title,description FROM articles WHERE area = ? AND category = ? AND hidden = 0;", array("ii", $area, $_GET['id']));
+  }
   while($row = $result->fetch_assoc()) {
     $id    = clean($row['id']);
     $title = clean($row['title']);
@@ -33,20 +43,6 @@ function drawCategory($mysqli, $area) {
   }
 }
 
-function drawCategorySlug($mysqli) {
-  $result = execPrepare($mysqli, "SELECT id,area,title,description FROM articles WHERE category = (SELECT id FROM categories WHERE slug = ?) AND hidden = 0;", array("s", $_GET['slug']));
-  while($row = $result->fetch_assoc()) {
-    $id    = clean($row['id']);
-    $title = clean($row['title']);
-    $desc  = clean($row['description']);
-
-    print '<div class="content-block">';
-    print '<a href="/article.php?id=' . $id . '">';
-    print '<h5 class="nopad-text">' . $title . '</h5>';
-    print '<p class="nopad-text">' . $desc . '</p>';
-    print '</a></div>';
-  }
-}
 ?>
 <div class="page-wrapper">
   <div class="content">
@@ -54,14 +50,7 @@ function drawCategorySlug($mysqli) {
       <?php drawCategoryDesc($mysqli); ?>
     </div>
     <div class="content-full">
-      <?php
-if (isset($_GET['slug'])) {
-  drawCategorySlug($mysqli);
-}
-else {
-  drawCategory($mysqli, $area);
-}
-?>
+      <?php drawCategory($mysqli, $area); ?>
     </div>
   </div>
 </div>
