a19fedab329714a7ac085d590430148776693de6
holly <holly@localhost.localdomain>
2019-05-15 06:46:07 -0400

    adding article link support

--- functions.php
+++ functions.php
@@ -41,6 +41,7 @@ function display($input) {
   $input = str_replace("&lt;h3&gt;", "<h3>", $input);
   $input = str_replace("&lt;/h3&gt;", "</h3>", $input);
   $input = str_replace("&lt;br&gt;", "<br>", $input);
+  $input = preg_replace('&lt;a href="(https{0,1}:\/\/.*?)"&gt;(.*?)&lt;\/a&gt;','<a href="$1">$2</a>', $input)
   return $input;
 }
 
