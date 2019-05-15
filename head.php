7b22add14ced496e0d7e2cba46aa3c8975cd77b9
holly <holly@localhost.localdomain>
2019-05-15 08:57:56 -0400

    adding article link support

--- head.php
+++ head.php
@@ -1,7 +1,7 @@
 <html>
 <head>
  <title>CISO Journey</title>
- <link rel="stylesheet" type="text/css" href="/main.css" />
+ <link rel="stylesheet" type="text/css" href="/theme/main.css" />
  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
 </head>
