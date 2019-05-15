1c6183a700cb5e3866d625ceb706ceb9ff2b8352
holly <holly@localhost.localdomain>
2019-05-15 09:41:21 -0400

    adding cookie-law shenanigans

--- head.php
+++ head.php
@@ -7,5 +7,6 @@
 </head>
 <body>
 <div class="cookie-law">
-<p>We have placed cookies on your device to help make this website better. You can <a href="/cookies.php">click here</a> to change your cookie settings or learn more. <a href="/cookies.php?dismiss=true"><input class="admin-button" type="button" value="Dismiss"></a></p>
+<div><p>We have placed cookies on your device to help make this website better. You can <a href="/cookies.php">click here</a> to change your cookie settings or learn more.</p></div>
+<div><input class="admin-button" type="button" value="Dismiss"></div></p>
 </div>
